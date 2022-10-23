6b6ecc7b8e8b3ed5c9ed9c01977a94452029e1b2
kvaillant <vaillant.karl.971@gmail.com>
2022-10-24 01:03:42 +0200

    lottery service impl

--- services/ILotteryService.ts
+++ services/ILotteryService.ts
@@ -1,3 +1,11 @@
+import { ethers } from "ethers";
+
 export interface ILotteryService {
-  
+  isOwner(signer: ethers.Signer): Promise<boolean>;
+  buyTokens(signer: ethers.Signer, amount: number): Promise<string>;
+  bet(signer: ethers.Signer, amount: number): Promise<string>;
+  ownerWithdraw(signer: ethers.Signer, amount: number): Promise<string>;
+  burnTokens(signer: ethers.Signer, amount: number): Promise<string>;
+  claimPrize(signer: ethers.Signer, amount: number): Promise<string>;
+  displayPrize(signer: ethers.Signer): Promise<string>;
 }
